503d707782879fd6f55fc7f2b34c52b00cb51460
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-07 21:15:01 +0700

    UPDATE App to set state on change text

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    location: ''
+  };
+
   fetchData = (event) => {
     event.preventDefault();
     console.log('fetch data!');
+  };
+
+  changeLocation = (event) => {
+    this.setState({
+      location: event.target.value
+    });
   };
 
   render() {
@@ -14,7 +24,12 @@ class App extends Component {
         <h1>Weather</h1>
         <form onSubmit={this.fetchData}>
           <label>I want to know the weather for
-            <input placeholder={"City, Country"} type="text" />
+            <input
+              placeholder={"City, Country"}
+              type="text"
+              value={this.state.location}
+              onChange={this.changeLocation}
+              />
           </label>
         </form>
       </div>
