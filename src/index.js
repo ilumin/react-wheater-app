ca841c27bd61ad455cbdaa86d1122896a0606a48
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-08 09:00:51 +0700

    ADD empty store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 
+var store = createStore(function() {});
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
