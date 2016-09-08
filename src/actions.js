add19aa08e87ad116fb7933e3cd01fcf0fb0bf90
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-08 17:35:52 +0700

    UPDATE test case

--- src/actions.js
+++ src/actions.js
@@ -15,7 +15,6 @@ export function setSelectedTemp(temp) {
 }
 
 export function setData(data) {
-  console.log("SET DATA:", data);
   return {
     type: 'SET_DATA',
     data: data
@@ -23,7 +22,6 @@ export function setData(data) {
 }
 
 export function setDates(dates) {
-  console.log("SET DATES:", dates);
   return {
     type: 'SET_DATES',
     dates: dates
@@ -31,7 +29,6 @@ export function setDates(dates) {
 }
 
 export function setTemps(temps) {
-  console.log("SET TEMPS:", temps);
   return {
     type: 'SET_TEMPS',
     temps: temps
@@ -50,8 +47,6 @@ export function fetchData(url) {
     xhr({
       url: url
     }, function (err, data) {
-      console.log("FETCH SUCCESS:", data);
-
       var body = JSON.parse(data.body);
       var list = body.list;
       var dates = [];
