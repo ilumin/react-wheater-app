02fc08649bbe081fbd5aefda6206c8328e0a0980
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-08 15:44:30 +0700

    ADD test case

--- src/__tests__/actions.test.js
+++ src/__tests__/actions.test.js
@@ -8,6 +8,11 @@ describe('actions', () => {
     it('should have a type of "CHANGE_LOCATION"', () => {
       expect(changeLocation().type).toEqual('CHANGE_LOCATION');
     });
+
+    it('should pass on the location we pass in', () => {
+      var location = 'Bangkok, Thailand';
+      expect(changeLocation(location).location).toEqual(location);
+    });
   });
 
 });
