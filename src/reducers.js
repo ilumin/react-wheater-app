eb985b1ff7b412db0fc536c5f562e8ffdc6a3b4a
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-08 09:39:40 +0700

    ADD new reducer state

--- src/reducers.js
+++ src/reducers.js
@@ -15,7 +15,20 @@ export default function mainReducer(state = initState, action) {
       return Object.assign({}, state, {
         location: action.location
       });
-      break;
+    case 'SET_SELECTED_DATE':
+      return Object.assign({}, state, {
+        selected: {
+          temp: action.temp,
+          date: state.selected.date
+        }
+      });
+    case 'SET_SELECTED_TEMP':
+      return Object.assign({}, state, {
+        selected: {
+          date: action.date,
+          temp: state.selected.temp
+        }
+      });
     default:
       return state;
   }
