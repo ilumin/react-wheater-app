8bcd0ae4f0817c30762bdd34ba6503518a3111f0
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-08 16:06:55 +0700

    ADD test case CHANGE_LOCATION for reducer

--- src/__tests__/reducer.test.js
+++ src/__tests__/reducer.test.js
@@ -16,4 +16,21 @@ describe('mainReducer', () => {
     }));
   });
 
+  it('should react to an action with the type CHANGE_LOCATION', () => {
+    var location = 'Bangkok, Thailand';
+    expect(mainReducer(undefined, {
+      type: 'CHANGE_LOCATION',
+      location: location
+    })).toEqual(fromJS({
+      location: location,
+      data: {},
+      dates: [],
+      temps: [],
+      selected: {
+        date: '',
+        temp: null
+      }
+    }))
+  });
+
 });
