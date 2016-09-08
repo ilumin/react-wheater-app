98beb02dbc888bc45f41468c52ce481eb7ac2dd7
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-08 09:30:52 +0700

    ADD action

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,18 @@
 
+export function steSelectedDate(date) {
+  return {
+    type: 'SET_SELECTED_DATE',
+    date: date
+  }
+}
+
+export function steSelectedTemp(temp) {
+  return {
+    type: 'SET_SELECTED_TEMP',
+    temp: temp
+  }
+}
+
 export function changeLocation(location) {
   return {
     type: 'CHANGE_LOCATION',
